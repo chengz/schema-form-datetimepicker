c304186cdc6155bbe18b66f19e4a90861708ac46
Andreas Stricker <astricker@futurelab.ch>
2016-03-17 11:21:25 +0100

    Refactor datetimepicker into a directive and controller
    This fixes a problem when the control should emit a onChange event with
    ngModelOptions set to updateOn: blur. Without this refactoring the
    necessary $viewChangeListeners-listener would not fire on blur as the
    hidden input will never generate such an event.

--- src/schema-form-datetimepicker.js
+++ src/schema-form-datetimepicker.js
@@ -1,5 +1,6 @@
-angular.module('schemaForm-datetimepicker', ['schemaForm', 'mgcrea.ngStrap']).config(
-['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfPathProvider',
+angular.module('schemaForm-datetimepicker', ['schemaForm', 'mgcrea.ngStrap'])
+.config([
+  'schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfPathProvider',
   function(schemaFormProvider,  schemaFormDecoratorsProvider, sfPathProvider) {
 
     var picker = function(name, schema, options) {
@@ -19,4 +20,47 @@ angular.module('schemaForm-datetimepicker', ['schemaForm', 'mgcrea.ngStrap']).co
     'directives/decorators/bootstrap/strap/datetimepicker.html');
     schemaFormDecoratorsProvider.createDirective('datetimepicker',
     'directives/decorators/bootstrap/strap/datetimepicker.html');
-  }]);
+  }
+])
+
+.controller('asfDateTimeController', [
+    '$scope', function ($scope) {
+        var me = this;
+
+        me.timeChanged = function () {
+            $scope.ngModel.$setViewValue($scope.moment);
+        };
+
+        me.dateChanged = function () {
+            $scope.ngModel.$setViewValue($scope.moment);
+        };
+
+        $scope.moment = undefined;
+    }
+])
+
+.directive('asfDateTimePicker', [
+    function () {
+        return {
+            restrict: 'E',
+            scope: true,
+            templateUrl: 'directives/decorators/bootstrap/strap/datetimepickerdir.html',
+            replace: true,
+            bindToController: true,
+            controllerAs: 'ctrl',
+            controller: 'asfDateTimeController',
+            require: "ngModel",
+            link: function (scope, element, attrs, ngModel) {
+                // expose the model on scope
+                scope.ngModel = ngModel;
+
+                ngModel.$render = function () {
+                    if (ngModel.$viewValue) {
+                        scope.moment = ngModel.$viewValue;
+                    }
+                };
+            }
+        };
+    }
+]);
+
